7f6213843279b81d96df84f257f4e221e4f3593a
Graham Nott <gnott@starglobal.ca>
2018-12-05 18:19:12 -0800

    Basic parse JATS from a docx file and some test helper functions.

--- letterparser/parse.py
+++ letterparser/parse.py
@@ -1,4 +1,8 @@
 import pypandoc
 
-def dummy():
-    return True
+def raw_jats(file_name, root_tag="root"):
+    "convert file content to JATS"
+    jats_content = ''
+    output = pypandoc.convert_file(file_name, 'jats')
+    jats_content = "<%s>%s</%s>" % (root_tag, output.encode('utf8'), root_tag)
+    return jats_content
