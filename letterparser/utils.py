e6b2f541dd1f6247d1d0f450863de3afba31c4b8
Graham Nott <gnott@starglobal.ca>
2019-03-08 17:08:12 -0800

    Changed encoding decoding functions in utils.

--- letterparser/utils.py
+++ letterparser/utils.py
@@ -5,7 +5,20 @@ import sys
 
 
 def unicode_encode(string):
-    "encode to utf8 depending on the python version"
-    if sys.version_info[0] < 3:
+    """safely encode string as utf8 by catching exceptions"""
+    if string is None or isinstance(string, str):
+        return string
+    try:
         string = string.encode('utf8')
+    except (UnicodeDecodeError, TypeError, AttributeError):
+        string = unicode_decode(string)
+    return string
+
+
+def unicode_decode(string):
+    "try to decode from utf8"
+    try:
+        string = string.decode('utf8')
+    except (UnicodeEncodeError, AttributeError):
+        pass
     return string
