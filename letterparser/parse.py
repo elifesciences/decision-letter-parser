da987477eed3a36ec50df1ba92e35dbc567c3c55
Graham Nott <gnott@starglobal.ca>
2019-03-08 17:08:47 -0800

    Parse with docker pandoc.

--- letterparser/parse.py
+++ letterparser/parse.py
@@ -1,10 +1,38 @@
-import pypandoc
-from letterparser.utils import unicode_encode
+import os
+import docker
+from letterparser.utils import unicode_encode, unicode_decode
+
+
+DOCKER_IMAGE = "knsit/pandoc:v2.7"
+
+
+def get_docker_client():
+    return docker.from_env()
+
+
+def get_docker_volume_and_file(file_name):
+    parts = file_name.split(os.sep)
+    file_name_path = os.sep.join(parts[0:-1])
+    file_name_file = parts[-1]
+    return file_name_path, file_name_file
+
+
+def get_docker_volume(source_path, bind_path='/data', mode='ro'):
+    return {source_path: {'bind': bind_path, 'mode': mode}}
+
+
+def call_pandoc(file_name, output_format="jats"):
+    client = get_docker_client()
+    file_name_path, file_name_file = get_docker_volume_and_file(file_name)
+    volumes = get_docker_volume(file_name_path)
+    command = 'pandoc "/data/%s" -t %s' % (file_name_file, output_format)
+    output = client.containers.run(DOCKER_IMAGE, command, volumes=volumes)
+    return output
 
 
 def raw_jats(file_name, root_tag="root"):
     "convert file content to JATS"
     jats_content = ""
-    output = pypandoc.convert_file(file_name, "jats")
-    jats_content = "<%s>%s</%s>" % (root_tag, unicode_encode(output), root_tag)
+    output = call_pandoc(file_name)
+    jats_content = "<%s>%s</%s>" % (root_tag, unicode_encode(unicode_decode(output)), root_tag)
     return jats_content
