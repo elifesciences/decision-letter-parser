abb809bacd4c07d24ea6549791ada64421e80063
Graham Nott <gnott@starglobal.ca>
2019-09-19 09:04:10 -0700

    Install coveralls before tox is run in tests script.

--- project_tests.sh
+++ project_tests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
+pip install coveralls
 tox
 . .tox/py35/bin/activate
-pip install coverage
-pip install coveralls
 COVERALLS_REPO_TOKEN=$(cat /etc/coveralls/tokens/decision-letter-parser) coveralls
 
