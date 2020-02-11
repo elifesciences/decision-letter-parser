004e951d6919cf0d7f0e8d8de90a3cbb0f587627
Luke <lsh-0@users.noreply.github.com>
2020-02-11 12:03:51 +1030

    tox, changes py35 environment to just py3

--- project_tests.sh
+++ project_tests.sh
@@ -2,7 +2,7 @@
 set -e
 
 tox
-. .tox/py35/bin/activate
+. .tox/py3/bin/activate
 pip install coveralls
 COVERALLS_REPO_TOKEN=$(cat /etc/coveralls/tokens/decision-letter-parser) coveralls
 
--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(name='letterparser',
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3"
         ]
     )
