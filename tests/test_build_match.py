8f8ab3d3b9fb54e1b7d1f3afa2b7c46c534052e2
Graham Nott <gnott@starglobal.ca>
2020-11-26 15:10:00 -0800

    Allow a full stop at end of asset names.

--- tests/test_build_match.py
+++ tests/test_build_match.py
@@ -25,6 +25,10 @@ class TestMatchPatterns(unittest.TestCase):
             "content": "&lt;Decision letter image 666&gt;",
             "expected": True
         },
+        {
+            "content": "content &lt;Author response image 2.&gt;",
+            "expected": False
+        },
     )
     def test_match_fig_content_start(self, test_data):
         self.assertEqual(
@@ -52,4 +56,31 @@ class TestMatchPatterns(unittest.TestCase):
     def test_match_fig_content_title_start(self, test_data):
         self.assertEqual(
             build.match_fig_content_title_start(test_data.get('content')),
+            test_data.get('expected'))
+
+    @data(
+        {
+            "content": "",
+            "expected": False
+        },
+        {
+            "content": "&lt;Author response video 1&gt;",
+            "expected": True
+        },
+        {
+            "content": "content &lt;Author response video 1&gt;",
+            "expected": False
+        },
+        {
+            "content": "&lt;Decision letter video 666&gt;",
+            "expected": True
+        },
+        {
+            "content": "content &lt;Author response video 2.&gt;",
+            "expected": False
+        },
+    )
+    def test_match_video_content_start(self, test_data):
+        self.assertEqual(
+            build.match_video_content_start(test_data.get('content')),
             test_data.get('expected'))
